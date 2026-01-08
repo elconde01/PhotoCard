37c8dcd5f36fb03490f89ba89351ddd2cf313264
Martin <43607388+elconde01@users.noreply.github.com>
2026-01-08 14:01:28 -0300

    Refactor service worker caching logic

--- sw.js
+++ sw.js
@@ -1,19 +1,17 @@
-const CACHE_NAME = "sesiones-foto-cache-v1";
-const urlsToCache = [
-  "./index.html",
-  "./manifest.json",
-  "./sw.js",
-  "./icon-192.png",
-  "./icon-512.png"
+const CACHE_NAME = 'sesiones-foto-cache-v1';
+const ASSETS = [
+  './index.html',
+  './manifest.json',
+  './sw.js',
+  './icon-192.png',
+  './icon-512.png'
 ];
 
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
-  );
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
 });
 
-self.addEventListener("activate", event => {
+self.addEventListener('activate', event => {
   event.waitUntil(
     caches.keys().then(keys =>
       Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
@@ -21,7 +19,7 @@ self.addEventListener("activate", event => {
   );
 });
 
-self.addEventListener("fetch", event => {
+self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request).then(resp => resp || fetch(event.request))
   );
