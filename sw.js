c10a6a51d0ec6bb60eb003819d3f45d8546c53a3
Martin <43607388+elconde01@users.noreply.github.com>
2026-01-08 13:41:11 -0300

    Refactor service worker event listeners for clarity

--- sw.js
+++ sw.js
@@ -7,31 +7,22 @@ const urlsToCache = [
   "./icon-512.png"
 ];
 
-// Instalar Service Worker y cachear archivos
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(urlsToCache);
-    })
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
 });
 
-// Activar y limpiar cachés viejas
 self.addEventListener("activate", event => {
   event.waitUntil(
-    caches.keys().then(keys => {
-      return Promise.all(
-        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
-      );
-    })
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
   );
 });
 
-// Interceptar requests y servir desde caché
 self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request).then(resp => resp || fetch(event.request))
   );
 });
